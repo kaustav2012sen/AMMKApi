12e1aa015ab61e48953f6995c1039984faebf90f
Rahul Das Naskar <rahuldasnaskar@gmail.com>
2019-05-12 18:19:56 +0530

    Commit 05122019 deployment changes

--- InventoryManagementSystem/Controllers/ValuesController.cs
+++ InventoryManagementSystem/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace InventoryManagementSystem.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Welcome to Adi Mohini Mohan Kanjilal", "Api Powered by Magic Box" };
         }
 
         // GET api/values/5
